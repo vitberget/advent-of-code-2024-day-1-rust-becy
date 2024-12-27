bb0ccfdcb6f46242378de7bcd8d4ed4302cfa7a8
Kenneth Hedman <github@vitberget.se>
2024-12-27 19:31:26 +0100

    smooth objects

--- src/render/puzzle.rs
+++ src/render/puzzle.rs
@@ -22,11 +22,46 @@ pub fn setup_puzzle_ticker( mut commands: Commands,) {
     })
 }
 
+#[derive(Component)]
+pub struct SmoothObject {
+    pub index: usize,
+    pub from: Transform,
+    pub to: Transform,
+    pub timer: Timer,
+    pub time: usize
+}
+
+pub fn smooth_object(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut objects_query: Query<(&RenderObject, &mut Transform)>,
+    mut smooth_query: Query<(Entity, &mut SmoothObject)>
+) {
+    for (entity, mut smooth) in &mut smooth_query {
+        smooth.timer.tick(time.delta());
+        if smooth.timer.finished() {
+            commands.entity(entity).despawn();
+            for (o, mut t) in &mut objects_query {
+                if o.index == smooth.index {
+                    *t = smooth.to;
+                }
+            }
+        } else {
+            for (o, mut t) in &mut objects_query {
+                if o.index == smooth.index {
+                    let d = (smooth.timer.elapsed().as_millis() as f32) / (smooth.time as f32);
+                    t.translation = smooth.from.translation +  (smooth.to.translation - smooth.from.translation) * d;
+                }
+            }
+        }
+    }
+}
+
 pub fn step_trigger(
     mut commands: Commands,
     time: Res<Time>,
     mut player_query: Query<(&RenderPlayer, &mut Transform), Without<RenderObject>>,
-    mut objects_query: Query<(&RenderObject, &mut Transform)>,
+    objects_query: Query<(&RenderObject, &Transform)>,
     // mut objects_query: Query<(&RenderObject, &mut Transform), Without<RenderPlayer>>,
     mut warehouse: ResMut<Warehouse>,
     mut puzzle_ticker: ResMut<PuzzleSolvingTicker>,
@@ -49,9 +84,15 @@ pub fn step_trigger(
             if let Some(objects) = moved_objects {
                 for (idx, pos) in objects {
                     warehouse.objects.insert(idx, pos);
-                    for (o, mut t) in objects_query.iter_mut() {
+                    for (o, t) in &objects_query {
                         if o.index == idx {
-                            *t = object_transform(&pos, &warehouse);
+                            commands.spawn(SmoothObject {
+                                index: idx,
+                                from: *t,
+                                to: object_transform(&pos, &warehouse),
+                                timer: Timer::new(Duration::from_millis(400), TimerMode::Once),
+                                time: 400
+                            });
                         }
                     }
                 }
